08c94a7072d58f3a5c38ea06ad1f2772ca87365a
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-01-05 14:02:08 +0530

    StackV1.2

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class ValidParentheses {
+    public static boolean isValid(String str){
+        Stack <Character>s = new Stack <>();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == '(' || ch == '[' || ch =='{'){
+                s.push(ch);
+            }
+            else{
+                if(s.isEmpty()){
+                    return false;
+                }
+                if ((s.peek() == '(' && ch == ')') || (s.peek() == '[' && ch == ']')
+                        || (s.peek() == '{' && ch == '}')) {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(s.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Enter a string of brackets: ");
+            String str = sc.next();
+            if(isValid(str)){
+                System.out.println("String is valid");
+            }
+            else{
+                System.out.println("String is not valid");
+            }
+        }
+    }
+}
