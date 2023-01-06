e55be0ecf4d7a8a0962e3fecda8e4ebc3bc775bc
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-01-06 14:16:35 +0530

    Queue

---
+++ Queue/Implementation/QueueUsingArray.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class QueueUsingArray{
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1;
+        }
+
+        //add
+        public static void add(int data){
+            if(rear == size-1){
+                System.out.println("Queue is Full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i=0; i<rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+    public static void main(String args[]){
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
