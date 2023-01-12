c9de54c58fbbd5b542d8d16470e57a56400fa70c
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-01-12 14:20:12 +0530

    Queue V1.6

---
+++ Queue/FirstNonRepeatingLetter.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class FirstNonRepeatingLetter{
+    public static void printNonRepeating(String str){
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch - 'a']++;
+
+            while(!q.isEmpty() && freq[q.peek() - 'a'] > 1){
+                q.remove();
+            }
+
+            if(q.isEmpty()){
+                System.out.print(-1 + " ");
+            }
+            else{
+                System.out.print(q.peek() + " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Enter any String: ");
+            String str = sc.next();
+            printNonRepeating(str);  
+            // aabccxb;
+        }
+    }
+}
