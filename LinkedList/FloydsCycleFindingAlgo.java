cc2a104c57c944d5ee7d9060297906fbf0c1da46
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-01-23 19:57:57 +0530

    LL V1.6

---
+++ LinkedList/FloydsCycleFindingAlgo.java
@@ -0,0 +1,144 @@
+import java.util.*;
+public class FloydsCycleFindingAlgo {
+    public static class LinkedList {
+        public class Node {
+            int data;
+            Node next;
+
+            public Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public static Node head;
+        public static Node tail;
+        public static int size = 0;
+
+        // addLast
+        public void addLast(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+            size++;
+        }
+
+        // removeLast
+        public int removeLast() {
+            if (head == null) {
+                System.out.println("Empty LinkedList");
+                return -1;
+            }
+            int data = tail.data;
+            size--;
+            if (head == tail) {
+                head = tail = null;
+                return data;
+            }
+            Node temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            tail = temp;
+            temp.next = null;
+            return data;
+        }
+
+        // print
+        public void printLL() {
+            if (head == null) {
+                System.out.println("Empty LinkedList");
+                return;
+            }
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+            System.out.print("null");
+        }
+
+        // Reverse LL
+        public void reverse() {
+            Node prev = null;
+            Node curr = tail = head;
+            Node next;
+            while (curr != null) {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            head = prev;
+        }
+
+        // Detect a loop/cycle in a LL
+    public boolean isCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public static void main(String args[]) {
+    try (Scanner sc = new Scanner(System.in)) {
+        LinkedList ll = new LinkedList();
+        char ch;
+        // addLast
+        System.out.print("Want to enter data at the end of LinkedList? (y/n): ");
+        ch = sc.next().charAt(0);
+        while (ch == 'y') {
+            System.out.print("Enter data at the end of LinkedList: ");
+            int data = sc.nextInt();
+            ll.addLast(data);
+            System.out.print("Want to enter more data ? (y/n): ");
+            ch = sc.next().charAt(0);
+        }
+
+        // removeLast
+        System.out.print("Want to remove data from the end of LinkedList? (y/n): ");
+        ch = sc.next().charAt(0);
+        while (ch == 'y') {
+            System.out.println(ll.removeLast() + " get removed from LinkedList");
+            System.out.print("Want to remove more data ? (y/n): ");
+            ch = sc.next().charAt(0);
+        }
+
+        // print
+        System.out.print("Want to print LinkedList? (y/n): ");
+        ch = sc.next().charAt(0);
+        if (ch == 'y') {
+            System.out.println("LinkedList is: ");
+            ll.printLL();
+        }
+
+        // ReverseLL
+        System.out.print("\nWant to check weather a LinkedList contains loop/cycle or not? (y/n): ");
+        ch = sc.next().charAt(0);
+        if (ch == 'y') {
+            if(ll.isCycle()){
+                System.out.println("LinkedList contain loop/cycle");
+            }
+            else{
+                System.out.println("LinkedList do not contain loop/cycle");
+            }
+        }
+
+    } catch (Exception e) {
+        System.out.println(e);
+    }
+
+}
+}
