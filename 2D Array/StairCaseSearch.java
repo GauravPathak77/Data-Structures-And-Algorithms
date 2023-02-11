f97f8d7175496726a162d892846bef7b69e99e09
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-02-11 17:24:00 +0530

    2D Array V1.2

---
+++ 2D Array/StairCaseSearch.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class StairCaseSearch {
+    public static void stairCaseSearch(int matrix[][], int key){
+        int row = 0, col = matrix[0].length-1;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == key){
+                System.out.print("Element found at (" + row + "," + col + ")");
+                return;
+            }
+            
+            if(key < matrix[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        System.out.print("Element not found");
+    }
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("How many rows does the sorted matrix have? ");
+            int m= sc.nextInt();
+            System.out.print("How many columns does the sorted matrix have? ");
+            int n = sc.nextInt();
+            int matrix[][] = new int[m][n];
+            for(int i=0; i<m; i++){
+                for(int j=0; j<n; j++){
+                    System.out.print("Enter element at (" + i + "," + j + "): ");
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            System.out.print("Which element do you want to search? ");
+            int key = sc.nextInt();
+            stairCaseSearch(matrix,key);
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
