3e94cea3fcdfcd0115c4dde4ce978771a239d202
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-03-02 12:47:00 +0530

    Binary Trees V1.9

---
+++ Binary Trees/DiameterOfTree2.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class DiameterOfTree2 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        static int idx = -1;
+
+        public Node buildTree(ArrayList<Integer> nodes) {
+            idx++;
+            if (nodes.get(idx) == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes.get(idx));
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Height of tree
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    static class Info{
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+    // Diameter of tree
+    public static Info diameter(Node root) {
+        if (root == null) {
+            return new Info(0,0);
+        }
+
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam,rightInfo.diam), (leftInfo.ht + rightInfo.ht +1));
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+        return new Info(diam, ht);
+    }
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> nodes = new ArrayList<>();
+            System.out.println("Enter values of nodes and -1 for null: ");
+            char choice;
+            do {
+                int data = sc.nextInt();
+                nodes.add(data);
+                System.out.print("Do you Want to enter more values(y/n)? ");
+                choice = sc.next().charAt(0);
+            } while (choice == 'y' || choice == 'Y');
+
+            BinaryTree tree = new BinaryTree();
+            Node root = tree.buildTree(nodes);
+            System.out.print("\nDo you Want to print Diameter of tree(y/n)? ");
+            char ch = sc.next().charAt(0);
+            if (ch == 'y' || ch == 'Y') {
+                System.out.println("Diameter = " + diameter(root).diam);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
