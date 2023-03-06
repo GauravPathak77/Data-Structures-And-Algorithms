8a93e6403704ee3e60ecbc9ba96b8e0552cb7592
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-03-06 09:55:22 +0530

    Binary Trees V1.15

---
+++ Binary Trees/TransformToSumTree.java
@@ -0,0 +1,84 @@
+import java.util.*;
+public class TransformToSumTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        static int idx = -1;
+
+        public Node buildTree(ArrayList<Integer> nodes) {
+            idx++;
+            if (nodes.get(idx) == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes.get(idx));
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Transform
+    public static int transform(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+        
+        int data = root.data;
+        
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+
+        root.data = leftChild + newLeft + rightChild + newRight;
+
+        return data;
+    }
+
+    // Print Preorder
+    public static void preorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> nodes = new ArrayList<>();
+            System.out.println("Enter values of nodes and -1 for null: ");
+            char choice;
+            do {
+                int data = sc.nextInt();
+                nodes.add(data);
+                System.out.print("Do you Want to enter more values(y/n)? ");
+                choice = sc.next().charAt(0);
+            } while (choice == 'y' || choice == 'Y');
+
+            BinaryTree tree = new BinaryTree();
+            Node root = tree.buildTree(nodes);
+            System.out.print("\nDo you Want to transform tree into sum tree(y/n)? ");
+            char ch = sc.next().charAt(0);
+            if (ch == 'y' || ch == 'Y') {
+                transform(root);
+                preorder(root);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    } 
+}
