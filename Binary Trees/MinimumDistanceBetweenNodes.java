394bf298a84c0ccd8415eeec54bbb7e35af0558c
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-03-14 10:33:22 +0530

    Binary Trees V1.15

---
+++ Binary Trees/MinimumDistanceBetweenNodes.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+public class MinimumDistanceBetweenNodes {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        static int idx = -1;
+
+        public Node buildTree(ArrayList<Integer> nodes) {
+            idx++;
+            if (nodes.get(idx) == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes.get(idx));
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    // Lowest Common Ancestor
+    public static Node lca(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca(root.left, n1, n2);
+        Node rightLca = lca(root.right, n1, n2);
+
+        if (rightLca == null) {
+            return leftLca;
+        }
+        if (leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
+
+    // Distance between lca and node
+    public static int lcaDist(Node root, int n){
+        if(root == null){
+            return -1;
+        }
+
+        if(root.data == n){
+            return 0;
+        }
+
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+
+        else if(rightDist == -1){
+            return leftDist + 1;
+        }
+        else{
+            return rightDist + 1;
+        }
+    }
+
+    // Minimum Distance betwween Nodes
+    public static int minDist(Node root, int n1, int n2){
+        Node lca = lca(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> nodes = new ArrayList<>();
+            System.out.println("Enter values of nodes and -1 for null: ");
+            char choice;
+            do {
+                int data = sc.nextInt();
+                nodes.add(data);
+                System.out.print("Do you Want to enter more values(y/n)? ");
+                choice = sc.next().charAt(0);
+            } while (choice == 'y' || choice == 'Y');
+
+            BinaryTree tree = new BinaryTree();
+            Node root = tree.buildTree(nodes);
+            System.out.print("\nDo you Want to print minimum distance between two nodes of tree(y/n)? ");
+            char ch = sc.next().charAt(0);
+            if (ch == 'y' || ch == 'Y') {
+                System.out.print("\nEnter first node: ");
+                int n1 = sc.nextInt();
+                System.out.print("Enter second node: ");
+                int n2 = sc.nextInt();
+                System.out.println("Minimum distance betwwen two nodes is: " + minDist(root, n1, n2));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
