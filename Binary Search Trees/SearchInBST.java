eab4a48b3f76a4ff103371f0de60898216783d60
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-04-06 12:19:08 +0530

    Binary Search Trees V1.1

---
+++ Binary Search Trees/SearchInBST.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class SearchInBST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static boolean search(Node root, int key){
+        if(root == null){
+            return false;
+        }
+        if(root.data == key){
+            return true;
+        }
+        if(root.data > key){
+            return search(root.left, key);
+        }
+        else{
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> values = new ArrayList<>();
+            char ch;
+            do {
+                System.out.print("Enter value: ");
+                int data = sc.nextInt();
+                values.add(data);
+                System.out.print("Want to enter more values(y/n)? ");
+                ch = sc.next().charAt(0);
+            } while (ch == 'y' || ch == 'Y');
+
+            Node root = null;
+            for (int i = 0; i < values.size(); i++) {
+                root = insert(root, values.get(i));
+            }
+            System.out.println("\nEnter the value you want to search: ");
+            int key = sc.nextInt();
+            if(search(root, key)){
+                System.out.println("Element found");
+            }
+            else{
+                System.out.println("Element not found");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
