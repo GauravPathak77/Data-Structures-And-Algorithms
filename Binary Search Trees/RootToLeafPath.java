6fcba39fa68cb8c79164fae7de2eb738a51d68bd
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-04-10 18:16:21 +0530

    Binary Search Trees V1.4

---
+++ Binary Search Trees/RootToLeafPath.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class RootToLeafPath {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    // Print Path
+    public static void printPath(ArrayList<Integer> path){
+        for(int i=0; i<path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("NULL");
+    }
+    // Print Root to leaf Path
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> values = new ArrayList<>();
+            char ch;
+            do {
+                System.out.print("Enter value: ");
+                int data = sc.nextInt();
+                values.add(data);
+                System.out.print("Want to enter more values(y/n)? ");
+                ch = sc.next().charAt(0);
+            } while (ch == 'y' || ch == 'Y');
+
+            Node root = null;
+            System.out.println("\nBinary Search Tree is: ");
+            for (int i = 0; i < values.size(); i++) {
+                root = insert(root, values.get(i));
+            }
+            inorder(root);
+
+            System.out.println("\nPath From root to leaf: ");
+            printRoot2Leaf(root, new ArrayList<>());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
