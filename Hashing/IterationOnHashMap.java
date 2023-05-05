e7ec8ada0ae466a84ae76f24bc9b7e889bd2cd11
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-05-05 17:08:30 +0530

    Hashing V1.0

---
+++ Hashing/IterationOnHashMap.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class IterationOnHashMap{
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+            HashMap<String, Integer> map = new HashMap<>();
+            char ch;
+            do{
+                System.out.print("Enter key on HashMap: ");
+                String key = sc.next();
+                System.out.print("Enter value for Key: ");
+                int value = sc.nextInt();
+                map.put(key,value);
+                System.out.println("Want to enter more values?(y/n): ");
+                ch = sc.next().charAt(0);
+            }while(ch == 'y' || ch == 'Y');
+            System.out.println("\nHashMap is: ");
+            Set<String> keys = map.keySet();
+            System.out.println(keys);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
