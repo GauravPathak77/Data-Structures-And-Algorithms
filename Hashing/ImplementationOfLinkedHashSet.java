8b7022aa1b69c4b82d6ea8a0f23d43fa79b2e7e8
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-05-11 10:08:07 +0530

    Hashing V1.5

---
+++ Hashing/ImplementationOfLinkedHashSet.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ImplementationOfLinkedHashSet {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            LinkedHashSet<String> lset = new LinkedHashSet<>();
+            char ch;
+            do {
+                System.out.print("Enter value in HashSet: ");
+                lset.add(sc.next());
+                System.out.print("Want to enter more values?(y/n): ");
+                ch = sc.next().charAt(0);
+            } while (ch == 'y' || ch == 'Y');
+
+            System.out.println("\nLinkedHashSet is: ");
+            Iterator<String> it = lset.iterator();
+            while(it.hasNext()){
+                System.out.print(it.next() + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
