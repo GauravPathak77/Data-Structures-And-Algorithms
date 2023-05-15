d771f9297fa70f8e360007be896a911ee1a20260
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-05-15 20:41:02 +0530

    Hashing V1.9

---
+++ Hashing/Find_Itinerary_For_Tickets.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Find_Itinerary_For_Tickets{
+    public static String getStart(HashMap<String, String> tickets){
+        HashMap<String, String> revMap = new HashMap<>();
+        for(String key : tickets.keySet()){
+            revMap.put(tickets.get(key), key);
+        }
+        for(String key : tickets.keySet()){
+            if(!revMap.containsKey(key)){
+                return key;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        try(Scanner sc = new Scanner(System.in)){
+            HashMap<String, String> tickets = new HashMap<>();
+            char ch;
+            do{
+                System.out.print("Enter starting point: ");
+                String from = sc.next();
+                System.out.print("Enter ending point: ");
+                String to = sc.next();
+                tickets.put(from,to);
+                System.out.print("Want to enter more journey?(y/n): ");
+                ch = sc.next().charAt(0);
+            }while(ch == 'y' || ch == 'Y');
+
+            System.out.println("\nItinearay is: ");
+            String start = getStart(tickets);
+            System.out.print(start);
+            for(String key : tickets.keySet()){
+                System.out.print(" -> " + tickets.get(start));
+                start = tickets.get(start);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
