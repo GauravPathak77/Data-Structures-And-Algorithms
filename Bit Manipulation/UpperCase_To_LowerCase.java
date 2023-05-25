88788b5c585a1c4077b35d0d3f82401366f07afe
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-05-25 16:05:52 +0530

    Bit Manipulation V1.0

--- Bit Manipulation/UpperCase_To_LowerCase.java
+++ Bit Manipulation/UpperCase_To_LowerCase.java
@@ -2,9 +2,14 @@ import java.util.*;
 public class UpperCase_To_LowerCase {
     public static void main(String[] args) 
     {
-        // Convert uppercase character to lowercase
-        for(char ch='A';ch<='Z';ch++) {
-            System.out.println((char)(ch|' '));
+        try(Scanner sc = new Scanner(System.in)){
+            // Convert uppercase character to lowercase
+            System.out.println("Characters in lowercase: ");
+            for (char ch = 'A'; ch <= 'Z'; ch++) {
+                System.out.print((char) (ch | ' ') + "  ");
+            }
+        }catch(Exception e){
+            System.out.println(e);
         }
     }
     
