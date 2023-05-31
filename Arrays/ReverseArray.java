29b76e587b7923ef59ffa57d9079ce6dc8e5f013
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-05-31 19:11:02 +0530

    DSA

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -10,19 +10,22 @@ public class ReverseArray {
     }
     
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int[20];
-        int size;
-        System.out.println("Enter Size of array: ");
-        size = sc.nextInt();
-        System.out.println("Enter Sorted elements in an array: ");
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-        reverseArr(arr, size);
-        System.out.println("Array After sorting: ");
-        for (int i = 0; i < size; i++) {
-            System.out.print(arr[i] + "  ");
+        try(Scanner sc = new Scanner(System.in)){
+            int arr[] = new int[20];
+            int size;
+            System.out.println("Enter Size of array: ");
+            size = sc.nextInt();
+            System.out.println("Enter Sorted elements in an array: ");
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            reverseArr(arr, size);
+            System.out.println("Array After sorting: ");
+            for (int i = 0; i < size; i++) {
+                System.out.print(arr[i] + "  ");
+            }
+        }catch(Exception e){
+            System.out.println(e);
         }
     }
 }
