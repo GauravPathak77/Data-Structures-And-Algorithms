86b13a0001ea386ba7e2d9244cdfb7473d156937
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-06-01 17:26:37 +0530

    Graphs V1.1

---
+++ Graphs/Breadth_First_Search.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Breadth_First_Search {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+    
+    // BFS
+    public static void bfs(ArrayList<Edge>[] graph){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[graph.length];
+        q.add(graph[0].get(0).src);
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(!vis[curr]){
+                System.out.print(curr + " ");
+                vis[curr] = true;
+                for(int i=0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("How many nodes Graph contain: ");
+            int v = sc.nextInt();
+            ArrayList<Edge>[] graph = new ArrayList[v];
+
+            for (int i = 0; i < v; i++) {
+                graph[i] = new ArrayList<>();
+            }
+            char ch;
+            do {
+                System.out.print("Enter value of source: ");
+                int src = sc.nextInt();
+                System.out.print("Enter value of destination: ");
+                int dest = sc.nextInt();
+                System.out.print("Enter value of weight: ");
+                int wt = sc.nextInt();
+                graph[src].add(new Edge(src, dest, wt));
+                System.out.print("Want to enter more values?(y/n): ");
+                ch = sc.next().charAt(0);
+            } while (ch == 'y' || ch == 'Y');
+
+            System.out.println("\nBreadth First Search: \n");
+            bfs(graph);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
